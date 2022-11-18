932fe465a1c9a53929eef39c6a72e3b042abaebd
ansas <ansasi1998@gmail.com>
2022-11-18 18:22:00 +0100

    experience added

--- src/container/index.js
+++ src/container/index.js
@@ -3,7 +3,7 @@ import Contact from "./Contact/Contact";
 import Header from "./Header/Header";
 import Skills from "./Skills/Skills";
 import Testimonial from "./Testimonial/Testimonial";
-// import Work from "./Work/Work";
+import Experience from "./Experience/Experience";
 import Portfolio from "./Portfolio/Portfolio";
 
-export { About, Contact, Header, Skills, Testimonial, Portfolio };
+export { About, Contact, Header, Skills, Experience, Testimonial, Portfolio };
