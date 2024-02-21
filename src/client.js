4779341b6e6efdff81103aa227eecbc9710a7386
ansasi <ansasi1998@gmail.com>
2024-02-21 18:58:27 +0100

    clientjs

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "5xe35dbs",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-11-15",
   useCdn: true,
